0db29952b656ae9b73e55f6053ab9cc948802547
SangSuantak <plsuantak@gmail.com>
2013-04-04 22:33:07 +0530

    Initial Commit

---
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myfirstapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    //This is fired once send button is clicked
+    public void sendMessage(View view) {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText)findViewById(R.id.edit_message);
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	startActivity(intent);
+    }
+    
+    //Do stuff when activity is paused
+    @Override
+    public void onPause() {
+    	super.onPause();
+    	// my codes
+    }
+    
+    //Do stuff when activity is resumed
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	// my codes
+    }
+    
+}
